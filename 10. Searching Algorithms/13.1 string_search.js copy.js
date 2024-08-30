c5714d372bb69bfbab420440f28479356b95cd5f
talhazaryabDA6~~ <zaryabdev404@gmail.com>
2024-08-30 16:56:45 +0500

    progress

--- 10. Searching Algorithms/13.1 string_search.js copy.js
+++ 10. Searching Algorithms/13.1 string_search.js copy.js
@@ -4,15 +4,15 @@ function naiveSearch(long, short) {
     for (let i = 0; i < long.length; i++) {
 
         for (let j = 0; j < short.length; j++) {
-
             if (long[i + j] !== short[j]) {
                 break;
             }
 
-            if (j === short.length - 1) {
+            if (j = short.length) {
                 count++;
             }
         }
+
     }
 
     return count;
--- index.js
+++ index.js
@@ -1,32 +1,24 @@
-// UNOPTIMIZED VERSION OF BUBBLE SORT
-function bubbleSort(arr) {
-    for (var i = arr.length; i > 0; i--) {
-        for (var j = 0; j < i - 1; j++) {
-            console.log(arr, arr[j], arr[j + 1]);
-            if (arr[j] > arr[j + 1]) {
-                var temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+function naiveSearch(long, short) {
+    let count = 0;
+
+    for (let i = 0; i < long.length; i++) {
+
+        for (let j = 0; j < short.length; j++) {
+            if (long[i + j] !== short[j]) {
+                break;
+            }
+
+            if (j = short.length) {
+                count++;
             }
         }
+
     }
-    return arr;
+
+    return count;
+
 }
 
-// ES2015 Version
-function bubbleSort(arr) {
-    const swap = (arr, idx1, idx2) => {
-        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-    };
-
-    for (let i = arr.length; i > 0; i--) {
-        for (let j = 0; j < i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(arr, j, j + 1);
-            }
-        }
-    }
-    return arr;
-}
-
-bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]);
+console.log(
+    naiveSearch("lorie loledlol", "asdas")
+);
