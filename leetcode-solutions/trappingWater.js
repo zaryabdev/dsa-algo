a8afe28c3d2ab7ed952b3c8412d5876f5945d603
talhazaryabDA6~~ <zaryabdev404@gmail.com>
2024-10-02 17:36:55 +0500

    trapping water

--- leetcode-solutions/trappingWater.js
+++ leetcode-solutions/trappingWater.js
@@ -69,6 +69,39 @@ var trap = function (height) {
 };
 
 
+// Cracking FAANG
+var trap = function (height) {
+    debugger;
+    let left = 0;
+    let right = height.length - 1;
+
+    let leftMax = 0;
+    let rightMax = 0;
+
+    let trapped = 0;
+
+    while (left < right) {
+        let currentLeft = height[left];
+        let currentRight = height[right];
+
+        leftMax = Math.max(leftMax, currentLeft);
+        rightMax = Math.max(rightMax, currentRight);
+
+        if (leftMax < rightMax) {
+            trapped += leftMax - currentLeft;
+            left++;
+        } else {
+            trapped += rightMax - currentRight;
+            right--;
+        }
+    }
+    debugger;
+    return trapped;
+
+};
+
+
+
 // algomap
 var trap = function (height) {
     const n = height.length;
@@ -122,36 +155,6 @@ var trap = function (height) {
         }
     }
     return res;
-};
-
-var trap = function (height) {
-    debugger;
-    let left = 0;
-    let right = height.length - 1;
-
-    let leftMax = 0;
-    let rightMax = 0;
-
-    let trapped = 0;
-
-    while (left < right) {
-        let currentLeft = height[left];
-        let currentRight = height[right];
-
-        leftMax = Math.max(leftMax, currentLeft);
-        rightMax = Math.max(rightMax, currentRight);
-
-        if (leftMax < rightMax) {
-            trapped += leftMax - currentLeft;
-            left++;
-        } else {
-            trapped += rightMax - currentRight;
-            right--;
-        }
-    }
-    debugger;
-    return trapped;
-
 };
 
 console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])); // 6
