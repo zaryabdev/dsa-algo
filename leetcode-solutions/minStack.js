beb32415cf754139f1fcc5cc4c455f3599ce438e
talhazaryabDA6~~ <zaryabdev404@gmail.com>
2024-10-03 13:20:27 +0500

    minStack

--- leetcode-solutions/minStack.js
+++ leetcode-solutions/minStack.js
@@ -1,8 +1,7 @@
 
-// https://leetcode.com/problems/min-stack/
-
 var MinStack = function () {
-
+    this.stack = [];
+    this.minStack = [];
 };
 
 /**
@@ -10,28 +9,39 @@ var MinStack = function () {
  * @return {void}
  */
 MinStack.prototype.push = function (val) {
-
+    if (this.stack.length == 0) {
+        this.minStack.push(val);
+    } else {
+        let lastValue = this.minStack[this.minStack.length - 1];
+        if (val < lastValue) {
+            this.minStack.push(val);
+        } else {
+            this.minStack.push(lastValue);
+        }
+    }
+    this.stack.push(val);
 };
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function () {
-
+    this.stack.pop();
+    this.minStack.pop();
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function () {
-
+    return this.stack[this.stack.length - 1];
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function () {
-
+    return this.minStack[this.minStack.length - 1];
 };
 
 /**
