2f268f6ed1b2d4ed48054949f43937d4254fb46f
talhazaryab <talhazaryabmail@gmail.com>
2024-10-06 23:52:58 +0500

    dailytemprature

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 
-var trap = function (height) {
+var dailyTemperatures = function (temperatures) {
+    let stack = [];
+    let res = new Array(temperatures.length).fill(0);
 
+    for (let i = 0; i < temperatures.length; i++) {
+        if (stack.length > 0) {
+            let [temp, index] = stack.pop();
+
+            if (temp > temperatures[i]) {
+                res[i] = index;
+            }
+
+        } else {
+            stack.push([temperatures[i], i]);
+        }
+    }
+
+
+    return res;
 
 };
 
-console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])); // 6
-
-// console.log(trap([4, 2, 0, 3, 2, 5])); // 9
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])); //[1,1,4,2,1,1,0,0]
---
+++ leetcode-solutions/dailyTemperatures.js
@@ -0,0 +1,6 @@
+
+var dailyTemperatures = function (temperatures) {
+
+};
+
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])); //[1,1,4,2,1,1,0,0]
