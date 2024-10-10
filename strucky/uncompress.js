dabf855528ddf16acf47f4399d1307b32c370814
talhazaryabDA6~~ <zaryabdev404@gmail.com>
2024-10-10 16:41:44 +0500

    uncompress

--- strucky/uncompress.js
+++ strucky/uncompress.js
@@ -1,9 +1,13 @@
+/***
+ * https://www.structy.net/problems/uncompress
+ * Write a function, uncompress, that takes in a string as an argument.
+ *  The input string will be formatted into multiple groups according to the following pattern:
+ */
 
 const uncompress = (s) => {
 
     let result = [];
     let i = 0, j = 0;
-
 
     while (j < s.length) {
         if (isInteger(s[j])) {
@@ -33,3 +37,4 @@ console.log(uncompress("2c3a1t")); // -> 'ccaaat'
 console.log(uncompress("4s2b")); // -> 'ssssbb'
 console.log(uncompress("2p1o5p")); // -> 'ppoppppp'
 console.log(uncompress("3n12e2z")); // -> 'nnneeeeeeeeeeeezz'
+
