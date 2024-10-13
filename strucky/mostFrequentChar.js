2d256fd36ff7b151f9891df71838f1187ad0bb22
talhazaryabDA6~~ <zaryabdev404@gmail.com>
2024-10-13 14:58:44 +0500

    mostFrequentChar

--- strucky/mostFrequentChar.js
+++ strucky/mostFrequentChar.js
@@ -4,18 +4,34 @@
  *  If there are ties, return the character that appears earlier in the string.
  */
 
+
 const mostFrequentChar = (s) => {
-    // todo
+    let hashMap = {};
+    ;
+    for (let i = 0; i < s.length; i++) {
+        hashMap[s[i]] = (hashMap[s[i]] || 0) + 1;
+    }
+    ;
+
+    let best = "";
+    for (let i = 0; i < s.length; i++) {
+        if (best == "" || hashMap[s[i]] > hashMap[best]) {
+            best = s[i];
+        }
+    }
+    return best;
+
+
 };
 
 
 
 console.log(
     mostFrequentChar('bookeeper'), // -> 'e'
-    // mostFrequentChar('david'), // -> 'd'
-    // mostFrequentChar('abby'), // -> 'b'
-    // mostFrequentChar('potato'), // -> 'o'
-    // mostFrequentChar('eleventennine'), // -> 'e'
-    // mostFrequentChar("riverbed"), // -> 'r'
+    mostFrequentChar('david'), // -> 'd'
+    mostFrequentChar('abby'), // -> 'b'
+    mostFrequentChar('potato'), // -> 'o'
+    mostFrequentChar('eleventennine'), // -> 'e'
+    mostFrequentChar("riverbed"), // -> 'r'
 
 );
