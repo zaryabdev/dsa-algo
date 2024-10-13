450950ae0bf2ad7e6fe8a4025acfe8a59f0d7bd4
talhazaryabDA6~~ <zaryabdev404@gmail.com>
2024-10-13 17:25:04 +0500

    linkedlist init

---
+++ strucky/3. Linked List/linked-list-values.js
@@ -0,0 +1,34 @@
+class Node {
+    constructor(data) {
+        this.val = data;
+        this.next = null;
+    }
+}
+
+const a = new Node("A");
+const b = new Node("B");
+const c = new Node("C");
+const d = new Node("D");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+console.log(JSON.stringify(a));
+console.log(JSON.stringify(b));
+console.log(JSON.stringify(c));
+
+//  A -> B -> C -> D
+
+const printLinkedList = (head) => {
+    let current = head;
+    console.log(JSON.stringify(current));
+    while (current !== null) {
+        if (current) {
+            console.log(current.val);
+            current = current.next;
+        }
+    }
+};
+
+printLinkedList(a);
