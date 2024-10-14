d0b4bb7d180698857df87f29271ccdeb006d71da
talhazaryabDA6~~ <zaryabdev404@gmail.com>
2024-10-14 05:28:56 +0500

    reverseList

---
+++ strucky/3. Linked List/reverseList.js
@@ -0,0 +1,58 @@
+/***
+ * Write a function, reverseList, that takes in the head of a linked list as
+ * an argument. The function should reverse the order of the nodes in the
+ * linked list in-place and return the new head of the reversed linked list.
+ * https://www.structy.net/problems/reverse-list
+ */
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const reverseList = (head) => {
+    let prev = null;
+    let current = head;
+
+    while (current !== null) {
+
+        let next = current.next;
+
+        current.next = prev;
+        prev = current;
+        current = next;
+
+
+    }
+
+    return prev;
+
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+
+// a -> b -> c -> d -> e -> f
+
+console.log(
+    reverseList(a) // f -> e -> d -> c -> b -> a
+);
+const p = new Node("p");
+
+// p
+
+console.log(
+    reverseList(p) // p
+);
