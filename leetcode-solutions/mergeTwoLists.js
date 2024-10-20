4f8c72f2d8174f6399ddf4f5ca36ed6de57cfff3
talhazaryabDA6~~ <zaryabdev404@gmail.com>
2024-10-20 18:09:00 +0500

    mergeTwoLists

--- leetcode-solutions/mergeTwoLists.js
+++ leetcode-solutions/mergeTwoLists.js
@@ -42,28 +42,65 @@ class LinkedList {
  *     this.next = (next===undefined ? null : next)
  * }
  *
- * @param {ListNode} node1
- * @param {ListNode} node2
+ * @param {ListNode} list1
+ * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function (node1, node2) {
-    let loco = new ListNode();
-    let current = loco;
 
-    while (node1 && node2) {
-        if (node1.val > node2.val) {
-            current.next = node2;
-            node2 = node2.next;
-        } else {
-            current.next = node1;
-            node1 = node1.next;
-        }
-        current = current.next;
-    }
-    current.next = node1 || node2;
 
-    return loco.next;
+// recursive
+var mergeTwoLists = function (list1, list2) {
+    if (list1 == null && list2 == null) return null;
+    if (list1 == null) return list2;
+    if (list2 == null) return list1;
+
+
+
 };
+
+// var mergeTwoLists = function (list1, list2) {
+//     const dummyHead = new ListNode();
+//     let tail = dummyHead;
+
+//     let curr1 = list1;
+//     let curr2 = list2;
+
+//     while (curr1 !== null && curr2 !== null) {
+//         if (curr1.val < curr2.val) {
+//             tail.next = curr1;
+//             curr1 = curr1.next;
+//         } else {
+//             tail.next = curr2;
+//             curr2 = curr2.next;
+//         }
+//         tail = tail.next;
+//     }
+
+//     if (curr1 == null) tail.next = curr2;
+//     if (curr2 == null) tail.next = curr1;
+
+//     return dummyHead.next;
+// };
+
+
+// var mergeTwoLists = function (node1, node2) {
+//     let loco = new ListNode();
+//     let current = loco;
+
+//     while (node1 && node2) {
+//         if (node1.val > node2.val) {
+//             current.next = node2;
+//             node2 = node2.next;
+//         } else {
+//             current.next = node1;
+//             node1 = node1.next;
+//         }
+//         current = current.next;
+//     }
+//     current.next = node1 || node2;
+
+//     return loco.next;
+// };
 
 const runCase = (array1, array2) => {
     const node1 = LinkedList.fromArray(array1).head;
