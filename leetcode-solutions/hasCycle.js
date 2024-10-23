f81e792973eed7c51884c5df992869f965026f4c
talhazaryabDA6~~ <zaryabdev404@gmail.com>
2024-10-23 14:41:27 +0500

    hasCycle

---
+++ leetcode-solutions/hasCycle.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/linked-list-cycle/
+
+var hasCycle = function (head) {
+    let slow = head;
+    let fast = head;
+
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow == fast) {
+            return true;
+        }
+    }
+    return false;
+};
